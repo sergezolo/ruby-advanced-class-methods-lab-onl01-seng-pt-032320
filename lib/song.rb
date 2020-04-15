f30834decc84cd21a7c099d5834458aff4189c88
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-15 21:38:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,9 +46,10 @@ class Song
   end
   
   def self.new_from_filename(string)
-    string.split("- ")
-    artist = string[0]
-    name = string[1].gsub!(".mp3", "")
+    rows = string
+    data = rows.split("- ")
+    artist = data[0]
+    name = data[1].gsub!(".mp3", "")
     
     song = self.new
     song.name = name
