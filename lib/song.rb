f8e9e8470f6b0a4901418ca56ed7bff7429f2a4b
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-15 21:12:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,12 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  
   end
   
 end
